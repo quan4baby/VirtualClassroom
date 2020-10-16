fd9c0b150dffba4e6474a17eb23d222692474374
Shaquan Patterson <pattersons3@chc.edu>
2020-10-16 14:08:52 -0400

    Worked on Create Announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,4 +2,21 @@ class AnnouncementsController < ApplicationController
 	def index
 		@announcements = Announcement.all
 	end
+
+	def new
+	end
+
+	def create
+		announcement = Announcement.new(announcement_params)
+		if announcement.save
+			redirect_to "/announcements"
+		else
+			redirect_to "/announcements/new"
+		end
+	end
+
+	private
+	def announcement_params
+		params.require(:announcement).permit(:title, :details)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 	get "announcements" => "announcements#index"
+	get "announcements/new" => "announcements#new"
+	post "announcements" => "announcements#create"
   end
